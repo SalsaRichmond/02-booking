7e3c70145aee0bdf0e3d29e4eb771dba8877aa0b
Salsa Guy Richmond <rodriguez2113@gmail.com>
2026-07-23 12:46:34 -0400

    Disable browser caching in Worker to force instant updates

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 export default {
   async fetch(request, env) {
-    return await env.ASSETS.fetch(request);
+    const response = await env.ASSETS.fetch(request);
+    const newHeaders = new Headers(response.headers);
+    newHeaders.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+    newHeaders.set('Pragma', 'no-cache');
+    newHeaders.set('Expires', '0');
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: newHeaders
+    });
   }
 };
