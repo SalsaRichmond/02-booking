568e539f84f1510b24c5ef3b53fba6c4b113107c
Salsa Guy Richmond <rodriguez2113@gmail.com>
2026-07-24 15:11:25 -0400

    Update all 7 photo performance items with authentic public Google Drive images

---
+++ .wrangler/tmp/deploy-UtT3Vg/index.js
@@ -0,0 +1,19 @@
+// index.js
+var index_default = {
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+    const newHeaders = new Headers(response.headers);
+    newHeaders.set("Cache-Control", "no-cache, no-store, must-revalidate");
+    newHeaders.set("Pragma", "no-cache");
+    newHeaders.set("Expires", "0");
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: newHeaders
+    });
+  }
+};
+export {
+  index_default as default
+};
+//# sourceMappingURL=index.js.map
